746a17865cf98669bbfaac0bc85f12f423482bb6
sucheth09 <sucheth891@gmail.com>
2026-08-04 11:10:43 +0530

    Question 1.py

--- Question 1/Question 1.py
+++ Question 1/Question 1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-# Dataset
+
 data = {
     'Area': [500, 700, 900, 1100, 1300, 1500, 1800, 2000],
     'Rooms': [1, 2, 2, 3, 3, 4, 4, 5],
@@ -13,18 +13,18 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Convert location into numerical values
+
 df = pd.get_dummies(df, columns=['Location'], dtype=int)
 
-# Input and output
+
 X = df[['Area', 'Rooms', 'Location_City', 'Location_Suburb']]
 y = df['Rent']
 
-# Create and train model
+
 model = LinearRegression()
 model.fit(X, y)
 
-# Predict rent for a new apartment
+
 new_apartment = [[1200, 3, 1, 0]]
 
 prediction = model.predict(new_apartment)
