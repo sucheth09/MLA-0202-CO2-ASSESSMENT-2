5e9338e7c9f398f447b0362ef2148254ff0d7f84
sucheth09 <sucheth891@gmail.com>
2026-08-04 11:09:18 +0530

    Question 3.py

--- Question 3/Question 3.py
+++ Question 3/Question 3.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 
-# Dataset
+
 data = {
     'Fever': [1, 1, 0, 0, 1, 0, 1, 0],
     'Cough': [1, 1, 1, 0, 1, 0, 1, 0],
@@ -11,23 +11,22 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Input features
+
 X = df[['Fever', 'Cough', 'Headache']]
 
-# Target
+
 y = df['Flu']
 
-# Create Naive Bayes model
+
 model = GaussianNB()
 
-# Train model
+
 model.fit(X, y)
 
-# New patient's symptoms
-# Fever = 1, Cough = 1, Headache = 1
+
 patient = [[1, 1, 1]]
 
-# Prediction
+
 prediction = model.predict(patient)
 
 if prediction[0] == 1:
