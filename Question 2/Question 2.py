e220e7c9866f42868d280d5451fa7276a935dae8
sucheth09 <sucheth891@gmail.com>
2026-08-04 11:05:56 +0530

    Question 2.py

--- Question 2/Question 2.py
+++ Question 2/Question 2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-# Dataset
+
 data = {
     'Free': [5, 4, 0, 1, 6, 0, 1, 5],
     'Money': [4, 3, 0, 1, 5, 0, 0, 4],
@@ -11,20 +11,20 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Input features
+
 X = df[['Free', 'Money', 'Offer']]
 
-# Target
+
 y = df['Spam']
 
-# Create and train model
+
 model = LogisticRegression()
 model.fit(X, y)
 
-# New email word frequencies
+
 new_email = [[4, 3, 2]]
 
-# Prediction
+
 prediction = model.predict(new_email)
 
 if prediction[0] == 1:
